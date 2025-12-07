055a88682aba9a1119ecd590897b0b20136ffba4
oyings <jagalbat13@naver.com>
2025-12-07 14:09:52 +0900

    deleted stuffs

--- calolink/src/main/java/shop/calolink/service/NutritionService.java
+++
@@ -1,7 +0,0 @@
-package shop.calolink.service;
-
-import shop.calolink.domain.dto.NutritionResponse;
-
-public interface NutritionService {
-    NutritionResponse getByFood(Long foodId);
-}
--- calolink/src/main/java/shop/calolink/service/PriceService.java
+++
@@ -1,6 +0,0 @@
-package shop.calolink.service;
-
-public interface PriceService {
-    //PriceResponse 구현 바람
-    PriceResponse getByFood(Long foodId);
-}
